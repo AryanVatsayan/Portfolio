c68833770270c070c758ad895aeb94c4ab9e6493
dashingvinit <21bcs7277@cuchd.in>
2023-09-22 18:52:58 +0530

    fixed css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import heroImage from "../assets/heroImage.png" ;
+import React from 'react';
+import heroImage from '../assets/heroImage.png';
 
 const Home = () => {
   return (
     <div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="text-5xl font-signature">Aryan</h1>
+          <h2 className="text-3xl font-medium">Full Stack Developer</h2>
+        </div>
+        <div className="flex justify-center items-center">
+          <img src={heroImage} alt="hero" className="w-96" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
