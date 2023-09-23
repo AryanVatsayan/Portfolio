9d5a6c395cf9ab9971694c4302a0cc1f56441cff
AryanVatsayan <ananaysaloh@gmail.com>
2023-09-23 20:28:48 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import SocialLinks from './components/SocialLinks';
+import Myabout from './components/Myabout';
+import Portfolio from './components/Portfolio';
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
+      <Myabout />
+      <Portfolio />
+
       <SocialLinks />
     </div>
   );
---
+++ src/components/Myabout.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Myabout = () => {
+  return (
+  <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to to-black text-white'>
+    
+    <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+            <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+        </div>
+
+        <p className='text-xl mt-20'>
+        I am a passionate individual who thrives on creativity and innovation.
+        With a strong sense of curiosity, I constantly seek to expand my knowledge and skills.
+        I believe in the power of positivity and strive to spread it wherever I go.
+        Whether through my work or personal endeavors, I aim to make a meaningful impact.
+        I cherish my relationships and value the importance of empathy and understanding.
+        Life, to me, is an exciting journey filled with opportunities for growth and discovery.
+        </p>
+        
+        <br/>
+
+        <p className='text-xl'>
+        I am a passionate individual who thrives on creativity and innovation.
+        With a strong sense of curiosity, I constantly seek to expand my knowledge and skills.
+        I believe in the power of positivity and strive to spread it wherever I go.
+        Whether through my work or personal endeavors, I aim to make a meaningful impact.
+        I cherish my relationships and value the importance of empathy and understanding.
+        Life, to me, is an exciting journey filled with opportunities for growth and discovery.
+        </p>
+    </div>
+  </div>
+)}
+
+export default Myabout;
